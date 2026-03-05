12e58c8cd871f976892c89191d21e7e974494154
natalylamus <natalylamusp@gmail.com>
2026-03-04 22:54:17 -0500

    Cambios en reportes

--- src/reportes.py
+++ src/reportes.py
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-# src/reportes.py
-=======
->>>>>>> 4226ac764d6c8aa9676dce9c105513f09f355adb
 from .portafolio import Portafolio
 
 
 class ReportadorFinanciero:
     """
-<<<<<<< HEAD
-    Responsable únicamente de generar reportes.
-    No guarda estado ni datos (SRP).
-=======
     Responsable únicamente de generar reportes financieros.
     Aplica el principio SRP (Single Responsibility Principle).
->>>>>>> 4226ac764d6c8aa9676dce9c105513f09f355adb
     """
 
     def imprimir_resumen(self, portafolio: Portafolio) -> None:
@@ -23,16 +14,6 @@ class ReportadorFinanciero:
         print("📊 RESUMEN DEL PORTAFOLIO")
         print("-" * 40)
 
-<<<<<<< HEAD
-        total = 0.0
-        for posicion in portafolio.posiciones:
-            valor = posicion.calcular_valor_actual(posicion.precio_entrada)
-            total += valor
-            print(
-                f"{posicion.instrumento.ticker} | "
-                f"{posicion.instrumento.tipo} | "
-                f"Cantidad: {posicion.cantidad} | "
-=======
         if not portafolio.posiciones:
             print("El portafolio no tiene posiciones.")
             return
@@ -51,16 +32,8 @@ class ReportadorFinanciero:
                 f"Sector: {instrumento.sector} | "
                 f"Cantidad: {posicion.cantidad} | "
                 f"Precio Entrada: {posicion.precio_entrada} | "
->>>>>>> 4226ac764d6c8aa9676dce9c105513f09f355adb
                 f"Valor: {valor}"
             )
 
         print("-" * 40)
-<<<<<<< HEAD
         print(f"Valor total del portafolio: {total}")
-
-
-
-=======
-        print(f"Valor total del portafolio: {total}")
->>>>>>> 4226ac764d6c8aa9676dce9c105513f09f355adb
